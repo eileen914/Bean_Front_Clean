ff7619ed273d946c82bd01e0f89a257fd768c8ff
eileen <eileen914@snu.ac.kr>
2025-08-13 00:10:29 +0900

    [SETTING] Copy code from setting_junha_0803 into merge-test-0813

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import DefaultLanding from './pages/DefaultLanding';
+import CafeLanding from './pages/CafeLanding';
+import CafeSignIn from './pages/CafeSignIn';
+import CafeRegister1 from './pages/CafeRegister1';
+import CafeRegister2 from './pages/CafeRegister2';
+import CafeMapCreating from './pages/CafeMapCreating';
+import CafeMapCreated from './pages/CafeMapCreated';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<DefaultLanding />} />
+          <Route path="/cafe-landing" element={<CafeLanding />} />
+          <Route path="/cafe-signin" element={<CafeSignIn />} />
+          <Route path="/cafe-register-1" element={<CafeRegister1 />} />
+          <Route path="/cafe-register-2" element={<CafeRegister2 />} />
+          <Route path="/cafe-map-creating" element={<CafeMapCreating />} />
+          <Route path="/cafe-map-created" element={<CafeMapCreated />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App; 
---
+++ src/pages/CafeSignIn.js
@@ -0,0 +1,54 @@
+import React from "react";
+import "./CafeSignIn.css";
+import { useNavigate } from "react-router-dom";
+
+const CafeSignIn = () => {
+  const navigate = useNavigate();
+
+  const handleSignIn = () => {
+    // TODO: 백엔드 연동 시 여기에 로그인 요청 코드 추가
+    console.log("로그인 시도");
+  };
+
+  const goToRegister = () => {
+    navigate("/cafe-register-1");
+  };
+
+  return (
+    <div className="cafe-signin">
+      {/* 고정 헤더 */}
+      <header className="register-fixed-header">
+        <div className="header-content">
+          <img src="/logo.png" alt="Bean Logo" className="header-logo" />
+          <h1 className="header-text">Bean</h1>
+        </div>
+      </header>
+
+      <div className="signin-box">
+        <h2>로그인</h2>
+        <input
+          type="text"
+          className="signin-input"
+          placeholder="아이디를 입력해주세요"
+        />
+        <input
+          type="password"
+          className="signin-input"
+          placeholder="비밀번호를 입력해주세요"
+        />
+        <div className="signin-subtext">아이디 찾기 ㅣ 비밀번호 찾기</div>
+
+        <div className="cafe-signin-buttons">
+          <button className="cafe-signin-btn login" onClick={handleSignIn}>
+            로그인
+          </button>
+          <button className="cafe-signin-btn register" onClick={goToRegister}>
+            업체 등록하기
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CafeSignIn;
