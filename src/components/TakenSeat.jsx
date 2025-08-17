ad47677a21eb638e8573797a072caec2a3a5cd5d
jdnjsy <jdnjsyoo@snu.ac.kr>
2025-08-17 11:41:41 +0900

    [Style] 예약기능 디자인 수정

--- src/components/TakenSeat.jsx
+++ src/components/TakenSeat.jsx
@@ -1,8 +1,25 @@
 
- import line12 from "../assets/Line7.png";
- import "./TakenSeat.css";
+import React, { useCallback, useState } from "react";
+import line12 from "../assets/Line7.png";
+import "./TakenSeat.css";
 
- export default function TakenSeat () {
+ export default function TakenSeat ( {defaultReserved = false, onToggle}) {
+  const [reserved, setReserved] = useState(defaultReserved);
+  const handleToggle = useCallback(() => {
+    setReserved((prev) => {
+      const next = !prev;
+      onToggle?.(next);        // 필요하면 부모에 알려줌(선택)
+      return next;
+    });
+  }, [onToggle]);
+  
+  const handleKey = (e) => {
+    if (e.key === "Enter" || e.key === " ") {
+      e.preventDefault();
+      handleToggle();
+    }
+  };
+
   return (
     <div className="taken-frame">
       <div className="taken-frame-wrapper">
@@ -11,38 +28,47 @@
             <img className="taken-line" alt="Line" src={line12} />
           </div>
         </div>
+      </div>
+
+      <div className="taken-div">
+        <p className="taken-element">
+          <span className="taken-text-wrapper">6-1</span>
+          <span className="taken-span">번 좌석</span>
+        </p>
+
+        <div className="taken-element-2">
+          2인석&nbsp;|&nbsp;&nbsp;기본(사각)
+          테이블&nbsp;|&nbsp;&nbsp;콘센트 자리
+        </div>
+
+        <div className="taken-div-wrapper-2">
+          <div className="taken-text-wrapper-2">0시간 38분동안 사용중</div>
+        </div>
+
+        <p className="taken-p">
+          <span className="taken-text-wrapper-3">
+            입장시간|&nbsp;&nbsp;15:03
+            <br />
+            예상 퇴장시간|&nbsp;&nbsp;17:33
+          </span>
+          <span className="taken-text-wrapper-4">&nbsp;</span>
+          <span className="taken-text-wrapper-4">(남은 시간 1시간 15분)</span>
+        </p>
+
+        {/* ▼ 같은 상자 토글 */}
+        <div
+          className={`taken-div-wrapper-3 ${reserved ? "reserved" : ""}`}
+          role="button"
+          tabIndex={0}
+          onClick={handleToggle}
+          onKeyDown={handleKey}
+          aria-pressed={reserved}
+        >
+          <div className="taken-text-wrapper-6">
+            {reserved ? "예약 완료" : "지금 예약하기"}
+          </div>
+        </div>
+      </div>
     </div>
-
-    <div className="taken-div">
-      <p className="taken-element">
-        <span className="taken-text-wrapper">6-1</span>
-
-        <span className="taken-span">번 좌석</span>
-      </p>
-
-      <div className="taken-element-2">
-        2인석&nbsp;|&nbsp;&nbsp;기본(사각)
-        테이블&nbsp;|&nbsp;&nbsp;콘센트 자리
-      </div>
-
-      <div className="taken-div-wrapper-2">
-        <div className="taken-text-wrapper-2">0시간 38분동안 사용중</div>
-      </div>
-
-      <p className="taken-p">
-        <span className="taken-text-wrapper-3">
-          입장시간|&nbsp;&nbsp;15:03
-          <br />
-          예상 퇴장시간|&nbsp;&nbsp;17:33
-        </span>
-        <span className="taken-text-wrapper-4">&nbsp;</span> 
-        <span className="taken-text-wrapper-4">(남은 시간 1시간 15분)</span>      
-      </p>
-
-      <div className="taken-div-wrapper-3">
-        <div className="taken-text-wrapper-6">자리 예약하기</div>
-      </div>
-    </div>
-  </div>
   );
- };
+}
--- src/components/UntakenSeat.jsx
+++ src/components/UntakenSeat.jsx
@@ -1,7 +1,25 @@
+import React, { useCallback, useState } from "react";
 import "./UntakenSeat.css";
 import line12 from "../assets/Line7.png";
 
  export default function UntakenSeat () {
+  const [reserved, setReserved] = useState(defaultReserved);
+
+  const handleToggle = useCallback(() => {
+    setReserved(prev => {
+      const next = !prev;
+      onToggle?.(next);
+      return next;
+    });
+  }, [onToggle]);
+
+  const handleKey = (e) => {
+    if (e.key === "Enter" || e.key === " ") {
+      e.preventDefault();
+      handleToggle();
+    }
+  };
+
   return (
     <div className="untaken-frame">
       <div className="untaken-frame-wrapper">
@@ -10,28 +28,36 @@ import line12 from "../assets/Line7.png";
             <img className="untaken-line" alt="Line" src={line12} />
           </div>
         </div>
-    </div>
-
-    <div className="untaken-div">
-      <div className="untaken-div-2">
-        <p className="untaken-element">
-          <span className="untaken-text-wrapper">6-1</span>
-
-          <span className="untaken-span">번 좌석</span>
-        </p>
-
-      <div className="untaken-element-2">
-        2인석&nbsp;|&nbsp;&nbsp;기본(사각)
-        테이블&nbsp;|&nbsp;&nbsp;콘센트 자리
       </div>
 
-        <div className="untaken-div-wrapper-2">
-          <div className="untaken-text-wrapper-2">자리 예약하기</div>
+      <div className="untaken-div">
+        <div className="untaken-div-2">
+          <p className="untaken-element">
+            <span className="untaken-text-wrapper">6-1</span>
+            <span className="untaken-span">번 좌석</span>
+          </p>
+
+          <div className="untaken-element-2">
+            2인석&nbsp;|&nbsp;&nbsp;기본(사각)
+            테이블&nbsp;|&nbsp;&nbsp;콘센트 자리
+          </div>
+
+          {/* ▼ 같은 박스에서 토글 */}
+          <div
+            className={`untaken-div-wrapper-2 ${reserved ? "reserved" : ""}`}
+            role="button"
+            tabIndex={0}
+            onClick={handleToggle}
+            onKeyDown={handleKey}
+            aria-pressed={reserved}
+          >
+            <div className="untaken-text-wrapper-2">
+              {reserved ? "예약 완료" : "지금 예약하기"}
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   );
- };
-
+}
 
