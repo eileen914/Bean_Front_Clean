574e0a97bb9d74b38da2483cb0687db931b04f91
jdnjsy <jdnjsyoo@snu.ac.kr>
2025-08-20 02:19:38 +0900

    [Style] gap

--- src/components/CafeList.jsx
+++ src/components/CafeList.jsx
@@ -1,14 +1,13 @@
 import PropTypes from "prop-types";
 import React from "react";
 import { useNavigate } from 'react-router-dom';
-import "./CafeList.css";
 import ellipse from '../assets/Ellipse.svg';
 import star_rating from '../assets/star_rating.svg';
 import ion_location from '../assets/ion_location.svg';
 import bookmark from '../assets/bookmark_sharp.svg';
 import coffee from '../assets/coffee-bean.svg';
 import mdi_clock from '../assets/mdi_clock.svg';
-import './UserChatbot.css';
+import "./CafeList.css";
 
 
 export const CafeList = ({
@@ -48,35 +47,34 @@ export const CafeList = ({
         </div>
       </div>
       <div className="list-frame-4">
-        <div className="list-frame-5">
-          <div className="list-frame-5">
-            <div className="list-frame-6">
-              <div className="list-frame-7">
-                <img className="list-vector-3" alt="Star" src={star_rating} />
-                <p className="list-p">
-                  <span className="list-text-wrapper-3">4.9 </span>
-                  <span className="list-text-wrapper-4">(1,067)</span>
-                </p>
-              </div>
-              <div className="list-div-wrapper">
-                <p className="list-div-2">
-                  <span className="list-text-wrapper-5">#</span>
-                  <span className="list-text-wrapper-6">&nbsp;</span>
-                  <span className="list-text-wrapper-5">카공</span>
-                </p>
-              </div>
+        {/* 평점/태그 영역 */}
+        <div className="list-rating-tag">
+          <div className="list-frame-6">
+            <div className="list-frame-7">
+              <img className="list-vector-3" alt="Star" src={star_rating} />
+              <p className="list-p">
+                <span className="list-text-wrapper-3">4.9 </span>
+                <span className="list-text-wrapper-4">(1,067)</span>
+              </p>
             </div>
-            <div className="list-frame-8">
-              <div className="ion-location-sharp">
-                <img className="list-vector-4" alt="location" src={ion_location} />
-              </div>
-              <div className="list-frame-8">
-                <div className="list-text-wrapper-7">{cafeAddress}</div>
-              </div>
+            <div className="list-div-wrapper">
+              <p className="list-div-2">
+                <span className="list-text-wrapper-5">#</span>
+                <span className="list-text-wrapper-6">&nbsp;</span>
+                <span className="list-text-wrapper-5">카공</span>
+              </p>
             </div>
           </div>
         </div>
-        <div className="list-frame-9">
+        {/* 주소 영역 */}
+        <div className="list-address">
+          <div className="ion-location-sharp">
+            <img className="list-vector-4" alt="location" src={ion_location} />
+          </div>
+          <div className="list-text-wrapper-7">{cafeAddress}</div>
+        </div>
+        {/* 영업시간 영역 */}
+        <div className="list-business-time">
           <img className="mdi-clock" alt="clock" src={mdi_clock} />
           <div className="frame-6">
             <div className="list-text-wrapper-8">영업종료</div>
